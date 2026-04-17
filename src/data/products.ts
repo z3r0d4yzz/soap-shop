3af5d1666f5f0cc1d2bfcf91e5e09c5b6b86c973
z3r0dayzz <armin.ottowitz@gmx.ch>
2026-04-17 20:59:32 +0200

    Erste Shop-Vorlage erstellt

---
+++ src/data/products.ts
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: "lavendel-seife",
+    name: "Lavendel Seife",
+    price: "CHF 12.90",
+    description: "Sanfte Platzhalterbeschreibung fuer das Produkt.",
+    longDescription:
+      "Dies ist eine ausfuehrlichere Platzhalterbeschreibung fuer die Lavendel Seife. Spaeter koennen hier Wirkung, Duft, Herstellung und Inhaltsstoffe stehen.",
+    image: "",
+    twintLink: "#",
+  },
+  {
+    id: "zitronen-seife",
+    name: "Zitronen Seife",
+    price: "CHF 11.90",
+    description: "Frische Platzhalterbeschreibung fuer das Produkt.",
+    longDescription:
+      "Dies ist eine ausfuehrlichere Platzhalterbeschreibung fuer die Zitronen Seife. Spaeter koennen hier Wirkung, Duft, Herstellung und Inhaltsstoffe stehen.",
+    image: "",
+    twintLink: "#",
+  },
+  {
+    id: "sensitive-seife",
+    name: "Sensitive Seife",
+    price: "CHF 13.90",
+    description: "Milde Platzhalterbeschreibung fuer empfindliche Haut.",
+    longDescription:
+      "Dies ist eine ausfuehrlichere Platzhalterbeschreibung fuer die Sensitive Seife. Spaeter koennen hier Wirkung, Duft, Herstellung und Inhaltsstoffe stehen.",
+    image: "",
+    twintLink: "#",
+  },
+];
