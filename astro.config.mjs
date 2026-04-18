9b7d358d3bb4a6380a51c6d9092cac585350e744
z3r0dayzz <armin.ottowitz@gmx.ch>
2026-04-18 17:19:19 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import tailwind from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://z3r0d4yzz.github.io",
-  base: "/soap-shop",
-  trailingSlash: "always",
+  base: "/soap-shop/",
   vite: {
     plugins: [tailwind()],
   },
